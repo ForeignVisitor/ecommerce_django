c1f047cae27684917809a49eebf7ae2ef974d9f4
Souhail Karam <77814484+ForeignVisitor@users.noreply.github.com>
2021-11-03 00:09:24 +0100

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('', include('landingPage.urls', namespace='landingPage')),
     path('store/', include('store.urls', namespace='store')),
     path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders'))
+    path('orders/', include('orders.urls', namespace='orders')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
